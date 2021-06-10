e067ff284167f8f6e5eb155f9fa21e6edd665775
GabeEscalaw <elise.escalaw@obf.ateneo.edu>
2021-06-10 18:38:51 +0800

    Updates

--- index.js
+++ index.js
@@ -2,78 +2,132 @@ import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/contro
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
-const scene = new THREE.Scene()
 
-const textureLoader = new THREE.TextureLoader()
-const myTexture = textureLoader.load('coolTex.jpg')
+// Here we always need 3 ObjectSpaceNormalMap
+// 1. Scene - Container that has all the objects, lights, etc
+// 2. Camera - To see what's in the scene
+// 3. Renderer - To actually render out the graphics in the scene and make the magic happen
 
-// Object
-const geometry = new THREE.BoxGeometry(1,1,1)
-const geometry2 = new THREE.DodecahedronGeometry(0.5,3)
-const material = new THREE.MeshBasicMaterial({
-    map: myTexture
-})
-const boxMesh = new THREE.Mesh(geometry,material)
-const sphereMesh = new THREE.Mesh(geometry2,material)
-scene.add(boxMesh)
-// scene.add(sphereMesh)
-boxMesh.position.x = 0
-boxMesh.position.y = 0.8
-sphereMesh.position.x = -1.6
-sphereMesh.position.y = 0.5
-geometry.center()
-// Sizes
-const sizes = {
-    width:window.innerWidth,
-    height:window.innerHeight
+// Set-up
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera ( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // ( FOV, Aspect Ratio, View Frustrum (near and far))
+
+const renderer = new THREE.WebGLRenderer({ // needs to know the domain to use which is the canvas with an id of #bg
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera ); // Renderer == Draw
+
+// Torus
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const material = new THREE.MeshStandardMaterial( { color: 0x00A7F0 } );
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20,20,20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight)
+
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper( 200, 50 );
+// scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Stars
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25);
+    const material = new THREE.MeshStandardMaterial( {color: 0xffffff} )
+    const star = new THREE.Mesh( geometry, material );
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+
+    star.position.set(x, y, z);
+    scene.add(star)
+
 }
 
-// Renderer gets updated each time window is resized
-window.addEventListener('resize',()=>{
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+Array(200).fill().forEach(addStar);
 
-    camera.aspect = sizes.width/sizes.height
-    camera.updateProjectionMatrix()
+// Background Image
 
-    renderer.setSize(sizes.width,sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-    
-})
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
-camera.position.z = 3
-scene.add(camera)
+// Cube with Picture
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
+const gabeTexture = new THREE.TextureLoader().load('creative Shot.jpg')
 
-controls.enableZoom = false;
-controls.enableDamping = true
+const gabe = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.MeshBasicMaterial( { map: gabeTexture } )
+);
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true,
-})
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+scene.add(gabe);
 
-const clock = new THREE.Clock()
+// Moon
 
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-    boxMesh.rotateX(30*0.0003)
-    boxMesh.rotateY(30*0.0003)
-    sphereMesh.rotateY(30*0.0003)
-    // mesh.position.y = Math.sin(elapsedTime) *0.1
-    boxMesh.position.z = Math.sin(elapsedTime) * 1
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 
-    controls.update()
-    controls.enableDamping = true
-    renderer.render(scene,camera)
-    window.requestAnimationFrame(tick)
-};
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial( {
+        map: moonTexture,
+        normalMap: normalTexture
+    } )
+);
 
-tick()
+scene.add(moon);
+
+moon.position.z = 30; // 1 
+moon.position.setX(-10); // 2
+
+gabe.position.x = 2;
+gabe.position.z = -5;
+// These are 2 ways of setting dimensions and they do the same thing
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top; 
+    // Gives the dimensions of the viewport, where the top property will show us exactly how far we are from the top of the webpage
+    // From there we can start changing properties on our 3D Objects whenever this function is called
+    // What's important here is changing the position of the actual camera (top value will always be negative, hence why we're multiplying it by a neg number )
+
+    moon.rotation.x += 0.05;
+    moon.rotation.x += 0.075;
+    moon.rotation.x += 0.05;
+
+    gabe.rotation.y += 0.01;
+    gabe.rotation.z += 0.01;
+
+    camera.position.x = t * -0.0002;
+    camera.position.y = t * -0.0002;
+    camera.position.z = t * -0.01;
+}
+
+document.body.onscroll = moveCamera
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    //controls.update();
+    renderer.render( scene, camera );
+}
+
+animate()
